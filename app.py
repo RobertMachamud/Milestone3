ea76e3b69575b97ae4133b403e8a7650941d871b
Robert Machamud <robert_machamud@yahoo.com>
2021-02-18 23:51:15 +0000

    adjusted index.htm

--- app.py
+++ app.py
@@ -12,10 +12,16 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello Hure"
+def index():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
