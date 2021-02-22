4caae183d9de6d872b4fb3caced874b92ed6fc5f
Robert Machamud <robert_machamud@yahoo.com>
2021-02-22 01:39:15 +0000

    added login, lregistration, logout, profile to app.py

--- app.py
+++ app.py
@@ -24,7 +24,109 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/regiser", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # checking if username already exist in db
+        user_exists = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()}
+        )
+
+        if user_exists:
+            flash("Sorry, this Username is already taken")
+            return redirect(url_for("register"))
+
+        register = {
+            "name": request.form.get("name").lower(),
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+            "email": request.form.get("email"),
+            "level": request.form.get(""),
+            "dob": request.form.get("dob"),
+            "about": request.form.get("about"),
+            "profile_img": "chef_profile.png",
+            "own_recipes": 0,
+            "saved_recipes": {}
+            # "reg_date": new Date() 
+        }
+
+        mongo.db.users.insert_one(register)
+
+        # putting new user into session cookie
+        session["user"] = request.form.get("username").lower()
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # checking if username in db
+        user_exists = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()}
+        )
+
+        if user_exists:
+            # checking if password is matching user input
+            if check_password_hash(
+                    user_exists["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                # flash()
+                return redirect(url_for("profile", username=session["user"]))
+            else:
+                flash("Username and/or Password incorrect")
+                return redirect(url_for("login"))
+
+        else:
+            # in case username doesn't exists
+            flash("Username and/or Password incorrect")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    # removing user from sessions cookies
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # use session user's username from db
+    user = mongo.db.users.find_one(
+        {"username": session["user"]}
+    )
+
+    username = user["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username, user=user)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+# questions:  1. Timestamp to database,  2. 
